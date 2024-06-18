fff06cf01ac8814429e156e94c8b95e6befbd3f0
王深浩 <wangshenhao@meituan.com>
2024-06-18 20:04:46 +0800

    feat: uodate

--- forge.config.js
+++ forge.config.js
@@ -45,4 +45,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'wsh694331170',
+          name: 'DEMO-WB'
+        },
+        prerelease: true,
+        draft: false
+      }
+    }
+  ],
 };
